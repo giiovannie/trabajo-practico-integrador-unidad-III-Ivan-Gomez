d6eacfbc6bf773cc16e59042dea8c8ca6529c0b3
ivan Emmanuel Gomez <ivanemmanuelgomez2017@gmail.com>
2026-06-05 14:40:19 -0300

    feat(app.js) se agrego la conexion con la api externa de los simpsons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
-const apiGeneral = "https://thesimpsonsapi.com/api/characters";
-const apiPersonaje = "https://thesimpsonsapi.com/api/characters/1"
+const URL_API = "https://thesimpsonsapi.com/api/characters";
 let fragment = document.createDocumentFragment();
+
+let persona = [];
+
+const traerPersonajes = async  () =>{
+    try {
+        const response = await fetch(URL_API);
+        const data = await response.json();
+        console.log(data)
+        console.log(data.results);
+        return data
+    } catch (error) {
+        console.log("tuvimos problemas, error: ", error)
+    }
+}
+
+
+const traerUnPersonaje = async (id)=>{
+    try {
+        const response = await fetch(`${URL_API}/${id}`)
+        const data = await response.json()
+
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+traerPersonajes()
+traerUnPersonaje(20)
