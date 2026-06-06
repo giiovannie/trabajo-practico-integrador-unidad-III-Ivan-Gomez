893bbec51a3f9edabcd22307313caa273e66c998
gomez ivan <ivanemmanuelgomez2017@gmail.com>
2026-06-06 14:22:07 -0300

    feat : se agrego una estructura y una logica para cargar las tarjetas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const URL_API = "https://thesimpsonsapi.com/api/characters";
-let fragment = document.createDocumentFragment();
-
+// let fragment = document.createDocumentFragment();
+const contenedorCards = document.getElementById("card-container")
 let capturadorPersonajes = [];
 
 const traerPersonajes = async  () =>{
@@ -32,11 +32,29 @@ const trabajarPersonaje = async () =>{
     let personajes = ObjetoPersonajes.results
     capturadorPersonajes = personajes
     console.log(capturadorPersonajes);
+
+    contenedorCards.textContent = "";
+
+    contenedorCards += capturadorPersonajes.forEach(card => {
+        contenedorCards.innerHTML += `
+            <div class="col-3">
+                <div class="card m-4 " style="width: 18rem">
+                    <img src="${card.img}" class="card-img-top" alt="${card.name}" />
+                    <div class="card-body">
+                        <h5 class="card-title">${card.name}</h5>
+                        <p class="card-text">Ocupacion: ${card.occupation}</p>
+                        <a href="#" class="btn btn-primary">ver detalle</a>
+                    </div>
+                </div>
+            </div>
+        `
+    });
 }
 
 
-const mostrarPersonaje = async () =>{
-    
-}
+// const mostrarPersonaje = async () =>{
+//     contenedorCards = ""
 
+//     contenedorCards += fo
+// }
 trabajarPersonaje()
