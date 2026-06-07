887bd84ae96bd8baa6f28daf27222dab78946490
gomez ivan <ivanemmanuelgomez2017@gmail.com>
2026-06-07 13:06:15 -0300

    feat(script.js) se agrego una logica carga datos a la ventana modal que tiene el html trabajado con eventos

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,9 @@ const btnBuscar = document.getElementById("btn-primary");
 const buscador = document.getElementById("buscador");
 const formSubmit = document.getElementById("form-buscar") //etiqueta form
 let datoBuscador = null;
-let inputLimpio = null
+let inputLimpio = null;
+const myModal = document.getElementById('miModal')
+const modalIntance = new bootstrap.Modal(myModal)
 
 
 formSubmit.addEventListener("submit", (e)=>{
@@ -27,6 +29,7 @@ const traerPersonajes = async  () =>{
         return data
     } catch (error) {
         console.log("tuvimos problemas, error: ", error)
+        document.write(`lo sentimos algo salio mal error 505`)
     }
 }
 
@@ -92,3 +95,29 @@ const loader = async () => {
 }
 loader()
 
+//seccion del modal
+
+contenedorCards.addEventListener("click", (e)=>{
+        const tarjeta = e.target.closest(".card")
+
+        if(!tarjeta) return;
+
+        const cardId = tarjeta.dataset.id
+        console.log(cardId)
+        const personajeCapturado = capturadorPersonajes.find(persona =>{
+            return persona.id === Number(cardId);
+        })
+
+        console.log(`coincidencia: ${personajeCapturado.id}`)
+
+        document.getElementById('modalNombre').textContent = personajeCapturado.name
+        document.getElementById('modalImg').src = `https://cdn.thesimpsonsapi.com/500/character/${personajeCapturado.id}.webp`
+        document.getElementById('modalEdad').textContent = personajeCapturado.age
+        document.getElementById('modalFecha').textContent = personajeCapturado.dateOfBirth
+        document.getElementById('modalGenero').textContent = personajeCapturado.gender
+        document.getElementById('modalOcupacion').textContent = personajeCapturado.occupation
+        document.getElementById('modalEstado').textContent = personajeCapturado.status
+        document.getElementById('modalFrase').textContent = personajeCapturado.phrases[0]
+
+        modalIntance.show()
+})
